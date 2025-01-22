7eeb78aba4b526022b3134625ed00a6ae8f0f30b
annapareddyjyothi <jyoannapareddy2004@gmail.com>
2025-01-22 15:28:36 +0530

    Update stringindexing.py

--- stringindexing.py
+++ stringindexing.py
@@ -10,10 +10,10 @@ print(credit_number[-4:])
 print(credit_number[::2])
 print(credit_number[::3])
 #exercise 1
-#credit_number = "1234-5678-9012-3456"
- #last_digits = credit_number[-4:]
- #print(f"XXXX-XXXX-XXXX-{last_digits}")
+credit_number = "1234-5678-9012-3456"
+last_digits = credit_number[-4:]
+print(f"XXXX-XXXX-XXXX-{last_digits}")
  #exercise 2
-#credit_number = "1234-5678-9012-3456"
-#credit_number = credit_number[::-1]
+credit_number = "1234-5678-9012-3456"
+credit_number = credit_number[::-1]
 print(credit_number)
