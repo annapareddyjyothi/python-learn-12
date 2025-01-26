b9e2b466d01f2aa0fb73acf254b6c138cdd57e57
annapareddyjyothi <jyoannapareddy2004@gmail.com>
2025-01-26 22:30:16 +0530

    listtuplesetdict.py

---
+++ listtuplesetdict.py
@@ -0,0 +1,9 @@
+my_list = [1, 2, 3, 4, 5]
+print(my_list)  # Output: [1, 2, 3, 4, 5]
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple) 
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+print(my_dict)  # Output: {'a': 1, 'b': 2, 'c': 3}
+my_set = {1, 2, 3, 4, 5}
+print(my_set)  # Output: {1, 2, 3, 4, 5}
+
